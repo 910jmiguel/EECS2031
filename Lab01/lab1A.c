d2e0f0c4238dc870eb4aec4c7d79a9ea803088bd
910jmiguela <910jmiguela@gmail.com>
2025-01-22 15:24:52 -0500

    Problem A completed, fix the -10000 break

--- Lab01/lab1A.c
+++ Lab01/lab1A.c
@@ -1,10 +1,10 @@
 /***************************************
-* Lab01 *
-* Author: Advincula, Juan-Miguel *
-* EECS/Prism username: jmiguela *
-* Yorku Student #: 220523627 *
-* Email: jmiguela@my.yorku.ca *
-****************************************/
+ * Lab01 *
+ * Author: Advincula, Juan-Miguel *
+ * EECS/Prism username: jmiguela *
+ * Yorku Student #: 220523627 *
+ * Email: jmiguela@my.yorku.ca *
+ ****************************************/
 
 #include <stdio.h>
 
@@ -12,47 +12,56 @@ int isDigit(char c);
 int isLetter(char c);
 int isOperator(char c);
 
-int main() {
+int isDigit(char c)
+{
+    return (c >= '0' && c <= '9');
+}
+
+int isLetter(char c)
+{
+    return (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z');
+}
+
+int isOperator(char c)
+{
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%');
+}
+
+int main()
+{
     int num;
     char ch;
-    //int sum;
+    int sum;
 
-    while(1) {
+    while (1)
+    {
         printf("Enter an integer and a character seperated by blank: ");
         scanf("%d %c", &num, &ch);
 
-        if(isDigit(1)) {
-            printf("Character '%c' represents a digit. Sum of %d and %c is %d\n\n", ch, num, ch, num + ch);
-        } else if(isLetter(ch)) {
-            printf("Character '%c' represents a letter\n\n", &ch);
-        } else if(isOperator(ch)) {
-            printf("Character '%c' represents an operator\n\n", &ch);
-        } else {
-            printf("Character '%c' represents others\n\n", &ch);
+        if (isDigit(ch))
+        {
+            int newNum = num - '0';
+            sum = newNum + ch;
+            printf("Character '%c' represents a digit. Sum of %d and %c is %d\n\n", ch, num, ch, sum);
+        }
+        else if (isLetter(ch))
+        {
+            printf("Character '%c' represents a letter\n\n", ch);
+        }
+        else if (isOperator(ch))
+        {
+            printf("Character '%c' represents an operator\n\n", ch);
+        }
+        else
+        {
+            printf("Character '%c' represents others\n\n", ch);
         }
 
-        if(num == -10000) {
+        if (num == -10000)
+        {
             break;
         }
     }
 
     return 0;
-}
-
-int isDigit(char c) {
-    if(c >= '0' && c <= '9') {
-        return c;
-    }
-}
-
-int isLetter(char c) {
-    if(c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
-        return c;
-    }
-}
-
-int isOperator(char c) {
-    if(c == '+' || c == '-' || c == '*' || c == '/' || c == '%') {
-        return c;
-    }
 }
