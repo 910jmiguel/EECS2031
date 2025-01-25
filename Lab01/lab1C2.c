e7c06d731df557a9aeafea889a039d6c77b99e19
910jmiguela <910jmiguela@gmail.com>
2025-01-25 10:59:42 -0500

    problem C2 completed

--- Lab01/lab1C2.c
+++ Lab01/lab1C2.c
@@ -14,10 +14,10 @@
 
 int isPrime(int n);
 
-main ()
+int main ()
 {
   int i;
-  int arr[SIZE];
+  int arr[SIZE] = {0};
   srand(time(0));
   for( i = 0; i<SIZE; i++){
     int ran = rand()%41 ;   // a random number that's in the range of [0,40] inclusive
@@ -31,19 +31,19 @@ main ()
   } 
   printf("]\n");
   // create two arrays of the same size of arr, one to hold even numbers and one for prime numbers; 
-  int even[SIZE];
-  int prime[SIZE];
+  int even[SIZE] ={0};
+  int prime[SIZE] = {0};
   int evenCount = 0;
   int primeCount = 0;
   // scan the array to find even and prime numbers, and put them in corresponding arrays
   for (i = 0; i < SIZE; i++) {
     if(arr[i] % 2 == 0) {
-      even[i] == arr[i];
+      even[evenCount] = arr[i];
       evenCount++;
     }
     
     if(isPrime(arr[i])) {
-      prime[i] == arr[i];
+      prime[primeCount] = arr[i];
       primeCount++;
     }
   }
@@ -53,16 +53,22 @@ main ()
   for(i = 0; i < evenCount; i++) {
     printf("%d ", even[i]);
   }
+  printf("\n");
+
   // output the prime numbers
   printf("%d prime numbers: ", primeCount);
   for(i = 0; i < primeCount; i++) {
     printf("%d ", prime[i]);
   }
+  printf("\n");
   return 0;
 }
 
-int isPrime(n) {
-  for (int i = 2; i < n; i++) {
+int isPrime(int n) {
+  if(n < 2) {
+    return 0;
+  }
+  for(int i = 2; i * i <= n; i++) {
     if (n % i == 0) {
       return 0;
     }
