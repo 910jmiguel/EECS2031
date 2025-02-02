da2adbfa416054602a8f51e32814850a67a8cfa8
910jmiguela <910jmiguela@gmail.com>
2025-02-01 23:46:16 -0500

    lab 2 completed

--- Lab02/lab2D.c
+++ Lab02/lab2D.c
@@ -2,18 +2,21 @@
 //your information here
 /***************************************
 * Lab02 *
-* Author: Last name, first name *
-* EECS/Prism username: Your eecs login username *
-* Yorku Student #: Your student number *
-* Email: Your email address *
+* Author: Advincula, Miguel *
+* EECS/Prism username: jmiguela *
+* Yorku Student #: 220523627 *
+* Email: jmiguela@my.yorku.ca *
 ****************************************/
 
 #include<stdio.h>
 //include string library header files
+#include<string.h>
 
 //more declarations as needed
 
 #define SIZE 35
+
+void my_strcat(char des[], char src[]);
 
 int main(){
    char a[SIZE];
@@ -28,8 +31,8 @@ int main(){
    scanf("%s",b);
    
    //Complete the Code
-   while (   ){
-    
+   while(strcmp(a, "xxx") != 0 && strcmp(b, "xxx") != 0) {
+
       strcpy(c,a); 
       strcpy(d,b);
 
@@ -39,11 +42,26 @@ int main(){
       printf("Original function strcat(a,b) results:   %s\n", a); 
       printf("My function my_strcat(c,d) results: %s\n\n", c);
 
-      //Take the inputs again till you enter "xxx" for both arrays 
+      printf("Enter the first string(with no spaces) stored in array 'a': ");
+      scanf("%s",a);
+
+      printf("Enter the first string(with no spaces) stored in array 'b': ");
+      scanf("%s",b);
    }
+   return 0;
 }
 
 //your version of strcat implementation here
 void my_strcat(char des[], char src[]){
+   int i, j;
+   for(i = 0; des[i] != '\0'; ++i) {
+   }
 
+   for(j = 0; src[j] != '\0'; j++) {
+      des[i + j] = src[j];
+   }
+
+   des[i + j] = '\0';
 }
+
+
--- Lab02/lab2E.c
+++ Lab02/lab2E.c
@@ -2,14 +2,14 @@
 //your info here
 /***************************************
 * Lab02 *
-* Author: Last name, first name *
-* EECS/Prism username: Your eecs login username *
-* Yorku Student #: Your student number *
-* Email: Your email address *
+* Author: Advincula, Miguel *
+* EECS/Prism username: jmiguela *
+* Yorku Student #: 220523627 *
+* Email: jmiguela@my.yorku.ca *
 ****************************************/
 
 #include<stdio.h>
-
+#include<string.h>
 // more header file and declarations as needed
 
 #define SIZE 36
@@ -17,6 +17,8 @@
 int main(){
    char a[SIZE];
    char b[SIZE];
+
+   int my_strcmp(char x[], char y[]);
    
    printf("Enter the first string(with no spaces) stored in array 'a': ");
    scanf("%s",a);
@@ -24,25 +26,37 @@ int main(){
    printf("Enter the first string(with no spaces) stored in array 'b': ");
    scanf("%s",b);
     
-   while (     ){
+   while (strcmp(a, "xxx") != 0 && strcmp(b, "xxx") != 0){
       int result = strcmp(a,b); 
-      if (  ) 
+      if (result < 0) 
          printf("strcmp:   \"%s\" appears earlier in dictionary than \"%s\"\n", a,b);
-      else if (  ) 
+      else if (result > 0) 
          printf("strcmp:   \"%s\" appears later in dictionary than \"%s\"\n", a,b);
       else 
          printf("\"%s\" and \"%s\" have same content\n", a, b);
       
       int result2 = my_strcmp(a,b);
-      if (  ) printf("mystrcmp: \"%s\" appears earlier in dictionary than \"%s\"\n\n", a,b);
-      else if (  ) printf("mystrcmp: \"%s\" appears later in dictionary than \"%s\"\n\n", a,b);
+      if (result2 < 0) printf("mystrcmp: \"%s\" appears earlier in dictionary than \"%s\"\n\n", a,b);
+      else if (result2 > 0) printf("mystrcmp: \"%s\" appears later in dictionary than \"%s\"\n\n", a,b);
       else printf("\"%s\" and \"%s\" have same content\n\n", a,b);
 
       //Ask for input again till "xxx" is entered for both arrays
+      printf("Enter the first string(with no spaces) stored in array 'a': ");
+      scanf("%s",a);
+
+      printf("Enter the first string(with no spaces) stored in array 'b': ");
+      scanf("%s",b);
    }
+   return 0;
 }
 
 // your version of strcmp implementation here
 int my_strcmp(char x[], char y[]){
-
+   int i;
+   for (i = 0; x[i] != '\0' || y[i] != '\0'; i++){
+      if (x[i] != y[i]){
+         return x[i] - y[i];
+      }
+   }
+   return 0;
 }
