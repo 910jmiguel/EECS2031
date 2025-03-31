0238a9e22714edc1f71b462696aee7d19e47bcb0
910jmiguel <910jmiguel@gmail.com>
2025-03-31 15:59:19 -0400

    Implement sum and difference calculations in lab5B.c; improve command-line argument handling and error messages

--- Practice_Codes/L5/lab5B.c
+++ Practice_Codes/L5/lab5B.c
@@ -24,31 +24,51 @@ int main(int argc, char *argv[]) {
    
     if (strcmp(argv[1], "sum") == 0) {
         printf("Calculating sum: ");
-        for (....................) {
-            ....................
+        for (int i = 2; i < argc; i++) {
+            printf("%s", argv[i]);
+            //if command-line arguments begin with sum list the integer literals seperated by +
+            if(i < argc - i) {
+                printf(" + ");
+            }
         }
         printf(" = %d\n", getSum(argv, argc));
     } 
-    else if (.....................) {
+    else if (strcmp(argv[1], "diff") == 0) {
         printf("Calculating difference: ");
-        for (..................) {
-            .....................
+        for (int i = 2; i < argc; i++) {
+            printf("%s", argv[i]);
+        // if command-line arguments begin with diff, list integer literals seperated by -
+            if(i < argc - i) {
+                printf(" - ");
+            }
         }
+
         printf(" = %d\n", getDiff(argv, argc));
     } 
     else {
-        printf(.............................);
-        ............
+        printf("Invalid operation. Use 'sum' or 'diff'.");
+        return 1;
     }
 
     return 0;
 }
 // Function to calculate the sum of integers
 int getSum(char *args[], int n) {
-    
+    // array of n char pointers where except the first two pointers all other pointers point to
+    // strings of integer literals, and then returns the sum of the integer literals
+    // use atoi function to convert command-line arguments which are provided as strings into integers
+    int sum = 0; 
+    for(int i = 2; i < n; i++) {
+        sum += atoi(args[i]);
+    }
+    return sum;
 }
 
 // Function to calculate the difference of integers
 int getDiff(char *args[], int n) {
-    
+    int diff = 0;
+    for(int i = 2; i < n; i++) {
+        diff += atoi(args[i]);
+    }
+    return diff;
 }
